4526f13ebc848f6ef5f76a706a40338d69e30078
jadewang425 <jadewang425@gmail.com>
2023-11-02 17:30:37 -0400

    added icons for social medias

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <h1 className="text-2xl font-bold">Hello</h1>
+      <Home />
     </div>
   );
 }
