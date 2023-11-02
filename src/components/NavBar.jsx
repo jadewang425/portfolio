d4a934166e014a9620534c030c7f1482dc7ab28c
jadewang425 <jadewang425@gmail.com>
2023-11-02 17:07:04 -0400

    added responsive navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 
 export default function NavBar() {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+
     return (
         <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
             <div>NavBar Logo</div>
@@ -15,16 +19,16 @@ export default function NavBar() {
 
 
             {/* Hamburger */}
-            <div className='hidden'>
-                <FaBars />
+            <div onClick={handleClick} className='md:hidden z-10'>
+                {!nav ? <FaBars />: <FaTimes />}
             </div>
             {/* Mobile menu */}
-            <ul className='hidden'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Projects</li>
-                <li>Contact</li>
+            <ul className={!nav ? 'hidden': 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+                <li className='py-6 text-4xl'>Home</li>
+                <li className='py-6 text-4xl'>About</li>
+                <li className='py-6 text-4xl'>Skills</li>
+                <li className='py-6 text-4xl'>Projects</li>
+                <li className='py-6 text-4xl'>Contact</li>
             </ul>
 
             {/* Social Icons */}
